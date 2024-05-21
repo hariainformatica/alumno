7f8e80129e7798100141d1ba53acf2dd49db8dd4
Emilio Haría <haria.informatica@gmailcom>
2024-05-21 08:16:26 +0100

    test_listaalumnos

--- tests/test_listaalumnos.py
+++ tests/test_listaalumnos.py
@@ -1,82 +1,32 @@
 from listaalumnos import ListaAlumnos
 import pytest
 
+@pytest.fixture
+def lista():
+    return ListaAlumnos()
+
 # Test de la clase ListaAlumnos
-def test_listaalumnos():
-    lista = ListaAlumnos()
+def test_listaAlumnos0(lista):
     assert len(lista) == 0
 
+def test_listaAlumnos1(lista):
     lista.agregar('Juan')
     assert len(lista) == 1
 
+def test_listaAlumno2(lista):
+    lista.agregar('Juan')
     lista.agregar('Pedro')
+
     assert len(lista) == 2
 
     assert lista[0] == 'Juan'
     assert lista[1] == 'Pedro'
 
-    lista[0] = 'Luis'
+def test_listaAlumnoDelete(lista):
+    lista.agregar('Luis')
     assert lista[0] == 'Luis'
 
     del lista[0]
-    assert len(lista) == 1
+    assert len(lista) == 0
 
-    assert 'Pedro' in lista
     assert 'Luis' not in lista
-
-    assert lista == ['Pedro']
-
-    lista.agregar('Juan')
-    assert lista == ['Pedro', 'Juan']
-
-    lista.delete('Juan')
-    assert lista == ['Pedro']
-
-    lista.delete('Juan')
-    assert lista == ['Pedro']
-
-    lista.delete('Pedro')
-    assert lista == []
-
-    lista.delete('Pedro')
-    assert lista == []
-
-    lista.agregar('Juan')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
-    lista.agregar('Maria')
-    lista.agregar('Carlos')
-    lista.agregar('Pedro')
-    lista.agregar('Luis')
-    lista.agregar('Ana')
