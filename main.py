2fa74d76febf9e2ab2e8cf1c26a38ac490fda229
Emilio Haría <haria.informatica@gmailcom>
2024-05-21 08:09:31 +0100

    ejemplo textual

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, VerticalScroll
-from textual.widgets import Button, Static
+from textual.binding import Binding
+from textual.widgets import Button, Static, Footer
 
 
 class ButtonsApp(App[str]):
     #CSS_PATH = "button.tcss"
+
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(
+            key="question_mark",
+            action="help",
+            description="Show help screen",
+            key_display="?",
+        ),
+        Binding(key="delete", action="delete", description="Delete the thing"),
+        Binding(key="j", action="down", description="Scroll down", show=False),
+    ]
+
 
     def compose(self) -> ComposeResult:
         yield Horizontal(
@@ -25,6 +39,8 @@ class ButtonsApp(App[str]):
                 Button.error("Error!", disabled=True),
             ),
         )
+
+        yield Footer()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         self.exit(str(event.button))
