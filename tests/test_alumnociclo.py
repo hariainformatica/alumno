9a669dff30297e19d73e41d0d7225e04949ebe30
Haria.Informatica <haria.informatica@gmail.com>
2024-06-07 12:40:41 +0100

    version 2 de test

--- tests/test_alumnociclo.py
+++ tests/test_alumnociclo.py
@@ -1,10 +1,14 @@
 import pytest
-
 from alumno.alumnociclo import AlumnoCiclo
 
+#------------------------
 @pytest.fixture
 def alumnociclo():
     return AlumnoCiclo('Juan', 'DAW', 1)
 
 def test_alumnociclo(alumnociclo):
     assert alumnociclo.read() == 'Juan|##|DAW|##|1'
+#---------------------------
+def test_2alumnociclo():
+    al = AlumnoCiclo('Juan', 'DAW', 1)
+    assert al.read() == 'Juan|##|DAW|##|1'
